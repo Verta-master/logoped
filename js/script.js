949d8ab232647e030e3f114124f9370bcdfd667e
Verta-master <vertyagina@gmail.com>
2018-10-29 16:03:42 +0600

    Menu adaptive

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@
 $('.menu__btn').click(function() {
   $('.menu').toggleClass('menu--opened');
   $('.menu__list').slideToggle();
-})
+});
+
+$('.menu__drop-btn').click(function() {
+  $(this).toggleClass('menu__drop-btn--up');
+  $(this).next().slideToggle();
+});
