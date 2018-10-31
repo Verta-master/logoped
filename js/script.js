685b26574b11be6ac464cc836b4636acf11cd732
Verta-master <vertyagina@gmail.com>
2018-10-31 12:53:22 +0600

    Update 2018-10-31T06:53:17.075Z

--- js/script.js
+++ js/script.js
@@ -8,3 +8,32 @@ $('.menu__drop-btn').click(function() {
   $(this).toggleClass('menu__drop-btn--up');
   $(this).next().slideToggle();
 });
+
+//Header scroll
+if (window.innerWidth < 766) {
+  $(window).scroll(function(){
+    if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+      $(".header").addClass('header--scroll');
+    } else {
+      $(".header").removeClass('header--scroll');
+    };
+  });
+};
+
+//Search
+$('.search__btn').click(function(e) {
+  e.stopPropagation();
+  $('.search__input').toggleClass('search__input--show');
+  $('.search__input').focus();
+});
+
+//Hide popups on escape and click outside
+$("body").click(function(){
+  $('.search__input').removeClass('search__input--show');
+});
+
+$("body").keydown(function(e) {
+  if (e.keyCode == 27) {
+    $('.search__input').removeClass('search__input--show');
+  }
+});
